ca5501d80e0eb3d6cc00cc51f1f5c865b2aac6af
khanrahim1 <rahimkhan231205@gmail.com>
2026-01-11 21:21:29 -0500

    fox deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,10 @@
-
 import axios from "axios";
 
-const envUrl = import.meta.env.VITE_API_URL;
-
-export const api = axios.create({
-  baseURL: envUrl ? envUrl.trim() : "http://localhost:5000/api",
-  headers: { "Content-Type": "application/json" },
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
+
 export default api;
